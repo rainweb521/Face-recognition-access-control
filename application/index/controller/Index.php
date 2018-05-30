217697d222df9368412d174cb28335cd22f797a7
rain <nylrain@163.com>
2018-05-30 08:45:48 +0800

    在人脸验证通过后，显示当前对比照片的相似度，方便测试和使用

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -27,7 +27,9 @@ class Index extends Common {
 //        return '<style type="text/css">*{ padding: 0; margin: 0; } .think_default_text{ padding: 4px 48px;} a{color:#2E5CD5;cursor: pointer;text-decoration: none} a:hover{text-decoration:underline; } body{ background: #fff; font-family: "Century Gothic","Microsoft yahei"; color: #333;font-size:18px} h1{ font-size: 100px; font-weight: normal; margin-bottom: 12px; } p{ line-height: 1.6em; font-size: 42px }</style><div style="padding: 24px 48px;"> <h1>:)</h1><p> ThinkPHP V5<br/><span style="font-size:30px">十年磨一剑 - 为API开发设计的高性能框架</span></p><span style="font-size:22px;">[ V5.0 版本由 <a href="http://www.qiniu.com" target="qiniu">七牛云</a> 独家赞助发布 ]</span></div><script type="text/javascript" src="http://tajs.qq.com/stats?sId=9347272" charset="UTF-8"></script><script type="text/javascript" src="http://ad.topthink.com/Public/static/client.js"></script><thinkad id="ad_bd568ce7058a1091"></thinkad>';
     }
     public function door(){
-
+//        $i=9;$j=10;
+//        echo (++$i)+$j;
+//        exit();
         return \view('door');
     }
     /**
@@ -50,10 +52,11 @@ class Index extends Common {
         $image1 = $image_src;
         $image2 = '';
         $state = 0;
+        $scoure = 0;
         /** state = 0 表示没有识别到人脸，  1 表示无人脸  2表示通过 */
         if ($result['error_code']==0){
-            $scoure = $result['result']['user_list'][0]['score'];
-            if ($scoure>70){
+            $scoure = floor($result['result']['user_list'][0]['score']);
+            if ($scoure>80){
                 $state = 2;
                 $user_id = $result['result']['user_list'][0]['user_id'];
                 $stuent_model = new StudentM();
@@ -66,7 +69,7 @@ class Index extends Common {
 //
 //            }
         }
-        $result = array('state'=>$state,'image1'=>$image1,'image2'=>$image2);
+        $result = array('state'=>$state,'image1'=>$image1,'image2'=>$image2,'scoure'=>$scoure);
         return $result;
 //        return '{"error_code":222202,"error_msg":"pic not has face","log_id":2659027871,"timestamp":1527231061,"cached":0,"result":null}';
     }
